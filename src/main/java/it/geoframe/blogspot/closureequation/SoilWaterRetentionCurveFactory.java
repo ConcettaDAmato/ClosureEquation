9a95b832d52a4927ab8b41e02187940e38090f6b
niccolotubini <tubini.niccolo@gmail.com>
2020-11-06 10:21:39 +0100

    Renamed the eclipse project.

---
+++ src/main/java/it/geoframe/blogspot/closureequation/SoilWaterRetentionCurveFactory.java
@@ -0,0 +1,49 @@
+/*
+ * GNU GPL v3 License
+ *
+ * Copyright 2019 Niccolo` Tubini
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package it.geoframe.blogspot.closureequation;
+
+/**
+ * @author Niccolo` Tubini
+ *
+ */
+
+public class SoilWaterRetentionCurveFactory {
+	
+	
+	public SoilWaterRetentionCurve create(String model) {
+		
+		SoilWaterRetentionCurve myModel = null;
+		
+		if(model.equalsIgnoreCase("Van Genuchten") || model.equalsIgnoreCase("VanGenuchten") || model.equalsIgnoreCase("VG")) {
+			myModel = new SWRCVanGenuchten();
+		}else if(model.equalsIgnoreCase("Brooks Corey") || model.equalsIgnoreCase("BrooksCorey") || model.equalsIgnoreCase("BC")) {
+			myModel = new SWRCBrooksCorey();
+		}else if(model.equalsIgnoreCase("Kosugi")) {
+			myModel = new SWRCKosugi();
+		}else if(model.equalsIgnoreCase("Romano")) {
+			myModel = new SWRCRomano();
+		}else {
+			System.out.println("\n\n\tERROR: please check swrcModel name.");
+		}
+
+		return myModel;
+	}
+
+}
---
+++ src/main/java/it/geoframe/blogspot/conductivitymodel/MualemKosugi.java
@@ -0,0 +1,65 @@
+/*
+ * GNU GPL v3 License
+ *
+ * Copyright 2019 Niccolo` Tubini
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * 
+ */
+package it.geoframe.blogspot.conductivitymodel;
+
+import org.apache.commons.math3.special.Erf;
+
+import it.geoframe.blogspot.closureequation.ClosureEquation;
+import it.geoframe.blogspot.conductivitymodel.ConductivityEquation;
+
+/**
+ * @author Niccolo` Tubini
+ *
+ */
+public class MualemKosugi extends ConductivityEquation{
+	
+	final double l = 0.5;   // Kosugi, 1996
+	final double gamma = 2; // Kosugi, 1996
+	final double eta = 1;   // Kosugi, 1996
+	private double saturationDegree = -999.0;
+	
+	public MualemKosugi(ClosureEquation rheology) {
+		super(rheology);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public double k(double x, double y, int id, int element) {
+			
+		saturationDegree = (super.rheology.f(x, y, id) - super.rheology.parameters.thetaR[id])/( super.rheology.parameters.thetaS[id] -  super.rheology.parameters.thetaR[id]); 
+		if(saturationDegree<1) {
+			return super.rheology.parameters.kappaSaturation[id] * Math.pow(this.saturationDegree, 0.5)*Math.pow( ( 0.5*Erf.erfc( Math.log(x/ super.rheology.parameters.par1[id])/(Math.sqrt(2)*super.rheology.parameters.par2[id]) + super.rheology.parameters.par2[id]/Math.sqrt(2)  ) ),2 );
+		} else {
+			return super.rheology.parameters.kappaSaturation[id];
+		}
+		
+	
+	}
+	
+	public double k(double x, int id, int element) {
+		
+		return -9999.0;
+	
+	}
+	
+}
